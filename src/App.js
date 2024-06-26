5d6510571005b89789765dc1e1423e4ccd04bf92
cesar0987 <cesarbenitez46@gmail.com>
2024-06-25 20:08:33 -0400

    feat:add a footer with this content

--- src/App.js
+++ src/App.js
@@ -18,6 +18,17 @@ function App() {
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </body>
+      <footer>
+        <p>
+          © 2024 [Company Name]. All rights reserved. [Company Name] | [Street
+          Address] | [City, State ZIP] Phone: (555) 123-4567 | Email:
+          info@companyname.com Privacy Policy | Terms of Service | Sitemap
+          Connect with us: [Social Media Icons: Facebook, Twitter, Instagram,
+          LinkedIn] Subscribe to our newsletter for updates and special offers.
+          [Email input field] [Subscribe button] Designed with ♥ in
+          [City/Country]
+        </p>
+      </footer>
     </div>
   );
 }
